9fe63eaca9432c68d73b322be9ceda1e41dd2e8c
SzczepanJany <szczepan.jany@gmail.com>
2021-03-10 19:40:07 +0100

    Connection

--- create_db.py
+++ create_db.py
@@ -1,4 +1,13 @@
 from psycopg2 import connect
+from connect_db import config
 
-def create_db():
-    con = connect
+params = config()
+conn = connect(**params)
+cur = conn.cursor()
+# execute a statement
+print('PostgreSQL database version:')
+cur.execute('SELECT version()')
+
+
+#def create_db():
+#    con = connect
