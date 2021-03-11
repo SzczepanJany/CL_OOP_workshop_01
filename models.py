0f7c7292a23fc5510b91d22a359a7d1fc57e125c
SzczepanJany <szczepan.jany@gmail.com>
2021-03-11 19:12:15 +0100

    loads

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from hashlib import new
 from connectdb import config_db
 from psycopg2 import connect, errors
 from passwords import hash_password
@@ -7,6 +8,35 @@ conn = connect(**params)
 conn.autocommit = True
 
 class users():
+
+    @staticmethod
+    def load_user_by_id(cursor, id_):
+        sql="""select id, username, hashed_password from users where id=%s"""
+        cursor.execute(sql, (id_,))
+        data = cursor.fetchone()
+        if data:
+            id_, username, hashed_password = data
+            loaded_user = users(username)
+            loaded_user._id = id_
+            loaded_user._hashed_password = hashed_password
+            return loaded_user
+        else:
+            return None 
+
+    @staticmethod
+    def load_user_by_username(cursor, username):
+        sql="""select id, username, hashed_password from users where username=%s"""
+        cursor.execute(sql, (username,))
+        data = cursor.fetchone()
+        if data:
+            id_, username, hashed_password = data
+            loaded_user = users(username)
+            loaded_user._id = id_
+            loaded_user._hashed_password = hashed_password
+            return loaded_user
+        else:
+            return None 
+
     def __init__(self, username='', password='', salt=''):
         self._id = -1
         self.username = username
@@ -30,10 +60,20 @@ class users():
 
     def save_to_db(self, cursor):
         if self._id == -1:
-            sql = """insert into users values (username, hased_password) values (%s, %s) returning id"""
+            sql = """insert into users (username, hashed_password) values (%s, %s) returning id"""
             values = (self.username, self.hashed_password)
             cursor.execute(sql,values)
-            self._id = cursor.fetchone()['id']
+            self._id = cursor.fetchone()[0]
             return True
         return False
     
+
+
+new_user = users('johny', 'tratata')
+cur = conn.cursor()
+new_user.save_to_db(cur)
+print(users.load_user_by_id(cur, 4))
+print(users.load_user_by_username(cur, 'johny'))
+
+print(users.load_user_by_id(cur, 2))
+print(users.load_user_by_username(cur, 'jony'))
